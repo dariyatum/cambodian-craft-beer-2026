80f1c7425706c6437ae3e397a55e3423d51e317f
Neath <neath@gmail.com>
2026-06-19 10:50:31 +0700

    nothing

---
+++ app/pages/schedule.vue
@@ -0,0 +1,44 @@
+<template>
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+
+
+<div class="main">
+        <section class="hero-section">
+                <img src="/images/bg-schedule.jpg" alt="">
+        </section>
+</div>
+
+
+
+</template>
+
+
+<style scoped>
+
+html, body {
+  font-family: 'Inter', sans-serif;
+}
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body{
+    background:
+        linear-gradient(
+            rgba(136, 87, 65, 0.88),
+            rgba(255, 212, 193, 0.88)
+        ),
+        url('https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b');
+
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+
+    min-height: 100vh;
+}
+
+</style>
