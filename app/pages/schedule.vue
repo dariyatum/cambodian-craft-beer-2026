9b2f4129aeecb11a540b2f21e42cbc94ea92e3e8
Neath <neath@gmail.com>
2026-06-20 10:36:39 +0700

    done 80% by neath

--- app/pages/schedule.vue
+++ app/pages/schedule.vue
@@ -4,22 +4,43 @@
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
+  <section class="hero">
+    <div class="image"><img src="/images/bg-schedule.jpg" alt=""></div>
 
-<section class="hero">
+    <div class="content">
+      <span class="title">Craft beer</span>
+      <h2>Even Schedule</h2>
+      <p>Join us for unforgettable beer experiences, brewery tours, tasting sessions, seasonal celebrations, and community gatherings. Whether you're a craft beer enthusiast or just beginning your journey, our events offer the perfect opportunity to explore unique flavors, meet fellow beer lovers, and create memorable moments.</p>
+    </div>
+  </section>
 
-  <div class="image">
-    <img src="/images/bg-schedule.jpg" alt="">
-  </div>
+    <section class="schedule-section">
+    <div class="container">
 
-  <div class="content">
 
-    <span class="title">Even Schedule</span>
-    <h2></h2>
-    <p></p>
+      <div class="schedule-list">
+        <EventScheduleItem
+          icon="fa-regular fa-clock"
+          title="Event Begins"
+          time="2:00 PM"
+        />
 
-  </div>
+        <EventScheduleItem
+          icon="fa-solid fa-users"
+          title="Introduction by the MC"
+          time="3:15 PM"
+        />
 
-</section>
+        <EventScheduleItem
+          icon="fa-solid fa-music"
+          title="DJ Wha-Wah"
+          time="3:30 PM"
+        />
+      </div>
+
+    </div>
+  </section>
+
 </template>
 
 
@@ -56,17 +77,14 @@ body{
   
 }
 
-
-
 .hero {
   display: flex;
   align-items: center;
   gap: 60px;
   max-width: 1200px;
-  margin: auto 100px;
+  margin: 100px auto;
   padding: 0 20px;
 }
-
 
 .image{
   flex: 1;
@@ -74,8 +92,8 @@ body{
 
 .image img{
   width: 100%;
-  display: block;
   border-radius: 20px;
+  display: block;
   box-shadow: 0 10px 30px rgba(0,0,0,0.2);
 }
 
@@ -84,22 +102,34 @@ body{
 }
 
 .title {
-  color: chocolate;
+  color: #d4a017;
   font-weight: 600;
   letter-spacing: 2px;
 }
 
-. h2 {
-  color: #000;
-  line-height: 1.8;
-
+.content h2 {
+  font-size: 2rem;
+  color: #222;
 }
 
-
 .content p{
-  color: #000;
+  color: #666;
   line-height: 1.8;
   margin-bottom: 30px;
 }
 
+.container {
+   width: 100%;
+   max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 1.5rem;
+    display: block;
+}
+
+
+.schedule-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
 </style>
