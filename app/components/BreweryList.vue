caa22da9baf4c93af7a011cfb519ac16c9207534
dariyatum <dariyatum412@gmail.com>
2026-06-22 09:04:31 +0700

    fix breweries

---
+++ app/components/BreweryList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="brewery-grid">
+    <BreweryCard
+      v-for="(brewery, index) in breweries"
+      :key="brewery.name"
+      :name="brewery.name"
+      :logo="brewery.logo"
+      :description="brewery.description"
+      :facebook="brewery.facebook"
+    />
+  </div>
+</template>
+
+<script setup>
+import breweries from '../data/breweries'
+</script>
